7304e399f3781001a24660be86b6af2793d66491
Robert Maculan <120978811+rmaculan@users.noreply.github.com>
2023-11-18 18:12:33 -0500

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -12,7 +12,7 @@ export const exOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': 'ddffb44edbmsh28c58ef9754b38dp144309jsn0893ed6f0135',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     },
   };
 
